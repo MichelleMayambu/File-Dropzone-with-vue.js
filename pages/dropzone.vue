38219e4b67ac908ba74e9ef35b20bd1ab8645659
MICHELLE MAYAMBU <michelle@netvendor.co.za>
2020-09-25 13:46:28 +0200

    file upload and display on webpage

---
+++ pages/dropzone.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1> Dropzone Page</h1>
+        <component :is="dropZoneComponent" />
+    </div>
+</template>
+
+<script>
+import dropZoneComponent from "~/components/dropZoneComponent.vue";
+export default {
+    components: {
+      dropZoneComponent
+    },
+    data() {
+        return {
+         dropZoneComponent
+        
+        }
+    }
+}
+</script>

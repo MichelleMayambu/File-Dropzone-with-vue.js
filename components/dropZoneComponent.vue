f039320684134e044b188734b5a2dacd17053bd0
MICHELLE MAYAMBU <michelle@netvendor.co.za>
2020-09-28 22:39:02 +0200

    fixed all errors regarding pdf file display

--- components/dropZoneComponent.vue
+++ components/dropZoneComponent.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
-        <v-card>
-        <v-toolbar outlined dense color="light-blue darken-1" dark>
+  <div>
+    {{ addedFiles }}
+    <v-card>
+      <v-toolbar outlined dense color="light-blue darken-1" dark>
         <v-toolbar-title>Upload document</v-toolbar-title>
         <v-spacer></v-spacer>
       </v-toolbar>
-        <br />
-        <br />  
-       <vue-dropzone
+      <br />
+      <br />
+      <vue-dropzone
         id="dropzone"
         ref="myVueDropzone"
         :options="dropzoneOptions"
@@ -17,48 +18,53 @@
         @vdropzone-success-multiple="success"
         @vdropzone-success="success"
       ></vue-dropzone>
-       <br />
-       <br />
-       <v-row>
-           <v-col cols="12" md="5">
-               <v-card>
-              <v-simple-table>
-                <template v-slot:default>
+      <br />
+      <br />
+      <v-row>
+        <v-col cols="12" md="5">
+          <v-card>
+            <v-simple-table>
+              <template v-slot:default>
                 <thead>
-                    <tr>
-                    <th class="text-left">{{headers.name}}</th>
-                    <th class="text-left">{{headers.delete}}</th>
-                    </tr>
+                  <tr>
+                    <th class="text-left">{{ headers.name }}</th>
+                    <th class="text-left">{{ headers.delete }}</th>
+                  </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(i, index) in addedFiles" :key="index"
-                   @click="selectedDocument = i.dataurl.dataURL">
-                    <td >{{i.title}}</td>
-                    <td><v-btn x-small dense rounded  color="red" dark>x</v-btn></td>
-                    </tr>
+                  <tr
+                    v-for="(i, index) in addedFiles"
+                    :key="index"
+                    @click="selectedfile = i.dataurl.dataURL"
+                  >
+                    <td>{{ i.title }}</td>
+                    <td>
+                      <v-btn x-small dense rounded color="red" dark>x</v-btn>
+                    </td>
+                  </tr>
                 </tbody>
-                </template>
+              </template>
             </v-simple-table>
-            </v-card>
-           </v-col>
-             <v-col cols="12" md="7">
-                 <div v-if="selectedDocument.length < 0">
-                     <v-card-title>Select file to preview</v-card-title>
-                 </div>
-                 <div v-else>
-                 <div v-if="selectedDocument.length > 0">
-                     <embed
-                      :src="selectedDocument"
-                       type="application/pdf"
-                      width= "100%"
-                      height= "500px"
-                      />
-                 </div>
-                 </div>
-           </v-col>
-       </v-row>
-        </v-card>
-    </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="7">
+          <div v-if="selectedfile.length == 0">
+            <v-card-title>Select file to preview</v-card-title>
+          </div>
+          <div v-else>
+            <div v-if="selectedfile.length > 0">
+              <embed
+                :src="selectedfile"
+                type="application/pdf"
+                width="100%"
+                height="500px"
+              />
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
 </template>
 
 <script src="~/components/dropzone.js"></script>
--- components/dropzone.js
+++ components/dropzone.js
@@ -1,91 +1,99 @@
-import vue2Dropzone from 'vue2-dropzone'
-import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
 
 export default {
-    name: 'app',
-    components: {
-      vueDropzone: vue2Dropzone
+  name: "app",
+  components: {
+    vueDropzone: vue2Dropzone,
+  },
+  data: function () {
+    return {
+      headers: {
+        name: "Name",
+        delete: "Delete",
+      },
+      fileUrl: [],
+      tempAttachments: [],
+      addedFiles: [],
+      selectedDocument: [],
+      selectedfile: [],
+      dropzoneOptions: {
+        // The Url Where Dropped or Selected files will be sent
+        url: " ",
+        maxFiles: 1,
+        // File Size allowed in MB
+        maxFilesize: 102400000,
+        // Authentication Headers like Access_Token of your application
+        // The way you want to receive the files in the server
+        paramName: "file",
+        acceptedFiles:
+          "image/jpeg,image/png,image/gif,image/*,application/pdf,image/jpeg, image/png,image/jpg",
+        dictDefaultMessage: "Drag and drop files here",
+        includeStyling: false,
+        previewsContainer: false,
+        thumbnailWidth: 250,
+        thumbnailHeight: 140,
+        uploadMultiple: true,
+        parallelUploads: 20,
+      },
+    };
+  },
+  computed: {
+    getAddedFiles() {
+      return this.selectedDocument;
     },
-    data: function () {
-      return {
-          headers:{
-           name : "Name",
-           delete : "Delete"
-          },
-          fileUrl:[],
-          tempAttachments:[],
-          addedFiles:[],
-          selectedDocument:[],
-          dropzoneOptions: {
-            // The Url Where Dropped or Selected files will be sent
-            url: " ",
-            maxFiles: 1,
-            // File Size allowed in MB
-            maxFilesize: 102400000,
-            // Authentication Headers like Access_Token of your application
-            // The way you want to receive the files in the server
-            paramName: "file",
-            acceptedFiles:
-              "image/jpeg,image/png,image/gif,image/*,application/pdf,image/jpeg, image/png,image/jpg",
-            dictDefaultMessage: "Drag and drop files here",
-            includeStyling: false,
-            previewsContainer: false,
-            thumbnailWidth: 250,
-            thumbnailHeight: 140,
-            uploadMultiple: true,
-            parallelUploads: 20
-          },
-      }
+    checkAddedFiles() {
+      return this.addedFiles;
     },
-    computed: {
-        getAddedFiles() {
-            return this.selectedDocument;
-        },
-        checkAddedFiles() {
-            return this.addedFiles;
-        },
-        checkAttachments(){
-            return tempAttachments;
-        }
+    checkAttachments() {
+      return this.tempAttachments;
     },
-    methods: {
-        fileAdded(file) {
-          this.fileUrl = file[0];
-          console.log("file droppped", this.fileUrl);
-          let fileData = file
-          let attachment = {}
-          this.getDataUrl();
-          attachment.id = fileData[0].uuid;
-          attachment.title = fileData[0].name;
-          attachment.type = "file";
-          attachment.extension = "." + file[0].type.split("/")[1];
-          attachment.user = JSON.parse(localStorage.getItem("user"));
-          attachment.content = "File Upload by Select or Drop";
-          attachment.thumb = file.dataURL;
-          attachment.thumb_list = file.dataURL;
-          attachment.dataurl = this.fileUrl;
-          attachment.isLoading = true;
-          attachment.progress = null;
-          attachment.size = file.size;
-          this.tempAttachments = [...this.tempAttachments, attachment];
-          this.addedFiles.push(attachment),
-          alert("file attached successfully");
-        },
-        getDataUrl() {
-         var filereader = new FileReader();
-         var base64;
-         filereader.onload = fileLoadedEvent => {
-            base64=fileLoadedEvent.target.result;
-            this.fileUrl.dataURL= base64;
-            filereader.readAsDataURL(this.fileUrl);
-         }
-        },
-        sendingFiles(files) {
-            console.log("files sending", files);
-        },
-        success(response) {
-            console.log("files uploaded successfully");
-            console.log("our response is =", response);
-        }
-    }
-  }
+  },
+  methods: {
+    getDataUrl() {
+      var fileReader = new FileReader();
+      var base64;
+      fileReader.onload = (fileLoadedEvent) => {
+        base64 = fileLoadedEvent.target.result;
+        this.fileUrl.dataURL = base64;
+      };
+      fileReader.readAsDataURL(this.fileUrl);
+    },
+    fileAdded(file) {
+      this.fileUrl = file[0];
+      console.log("file", file);
+      console.log("File Dropped => ", file[0]);
+      let fileData = file;
+      console.log("filedata", fileData);
+      this.getDataUrl();
+      let attachment = {};
+      attachment.id = fileData[0].uuid;
+      attachment.title = fileData[0].upload.filename;
+      attachment.type = "file";
+      attachment.extension = "." + file[0].type.split("/")[1];
+      attachment.user = JSON.parse(localStorage.getItem("user"));
+      attachment.content = "File Upload by Select or Drop";
+      attachment.thumb = file.dataURL;
+      attachment.thumb_list = file.dataURL;
+      attachment.dataurl = this.fileUrl;
+      attachment.isLoading = true;
+      attachment.progress = null;
+      attachment.entityTypeID = 0;
+      attachment.color = "success";
+      attachment.size = file.size;
+      this.tempAttachments = [...this.tempAttachments, attachment];
+      let data = new FormData();
+      this.addedFiles.push(attachment);
+      data.append("one file Data", attachment);
+      console.log(file.length);
+      file = [];
+    },
+    sendingFiles(files) {
+      console.log("files sending", files);
+    },
+    success(response) {
+      console.log("files uploaded successfully");
+      console.log("our response is =", response);
+    },
+  },
+};
